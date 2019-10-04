774200c30cdb886dbe3c7ff5282110048529bac3
greenpett <56070069+greenpett@users.noreply.github.com>
2019-10-04 23:09:07 +0200

    Complete Test for NumFinder
    2nd test practical.

--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -6,12 +6,12 @@ public class NumFinderMain {
         NumFinder nf = new NumFinder();
 
         // this works
-        // nf.find(new int[] {4, 25, 7, 9});
+         nf.find(new int[] {3, 7, 6, 8, 1});
 
         // this crashes
-        nf.find(new int[] {4, 3, 2, 1});
+        nf.find(new int[] {4, 3, 8, 2, 1, 40, 77});
 
-        System.out.println(nf.getLargest());
         System.out.println(nf.getSmallest());
+        System.out.println(nf.getLargest());
     }
 }
